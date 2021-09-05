eca17cf623055c69bc0abd1b54e227a86789c5c2
kedarmayekar <kedarmayekar077@gmail.com>
2021-09-05 18:43:49 +0530

    Authentication Token feature added for all guide routes

--- src/routes/guidesRoutes.js
+++ src/routes/guidesRoutes.js
@@ -6,6 +6,10 @@ const errorHandler = require('../middlewares/error');
 //JOI validation schema
 const schemas =  require('../helpers/schema');
 const validateData = require('../middlewares/validation');
+const AuthMiddleware = require('./../middlewares/auth');
+const auth = new AuthMiddleware();
+
+router.use(auth.authenticateToken);
 router.route('/')
 .post(validateData(schemas.guide),guideControllerObj.addGuide)
 .get(guideControllerObj.getGuides);
